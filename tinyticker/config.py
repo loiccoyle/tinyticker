03d2025dcfc4243601b6698b75e8310dac01d5c2
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-07 02:14:26 +0200

    fix(web): config file not found

--- tinyticker/config.py
+++ tinyticker/config.py
@@ -4,8 +4,11 @@ from .settings import CONFIG_FILE
 
 
 def read() -> dict:
-    with open(CONFIG_FILE, "r") as config_file:
-        return json.load(config_file)
+    if CONFIG_FILE.is_file()
+        with open(CONFIG_FILE, "r") as config_file:
+            return json.load(config_file)
+    else:
+        return {}
 
 
 def write(config: dict) -> None:
