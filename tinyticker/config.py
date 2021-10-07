42335e7d2bc0cf05fd6765abb902cd750772fead
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-07 04:24:39 +0200

    add loggin

--- tinyticker/config.py
+++ tinyticker/config.py
@@ -1,6 +1,9 @@
 import json
+import logging
 
 from .settings import CONFIG_FILE
+
+LOGGER = logging.getLogger(__name__)
 
 DEFAULT = {
     "api_key": None,
@@ -15,17 +18,21 @@ DEFAULT = {
 
 def read() -> dict:
     if CONFIG_FILE.is_file():
+        LOGGER.debug("Reading config file.")
         with open(CONFIG_FILE, "r") as config_file:
             return json.load(config_file)
     else:
-        return {}
+        LOGGER.debug("Fallback to default values.")
+        return DEFAULT
 
 
 def write(config: dict) -> None:
+    LOGGER.debug("Writing config file.")
     with open(CONFIG_FILE, "w") as config_file:
         json.dump(config, config_file, indent=2)
 
 
 # Write the default config
 if not CONFIG_FILE.is_file():
+    LOGGER.debug("No config file, creating default.")
     write(DEFAULT)
