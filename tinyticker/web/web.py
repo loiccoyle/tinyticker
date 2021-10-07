cefc5dd9fcd19a9b6f948178ab4a52389d42aced
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-07 02:37:00 +0200

    fix(web): fix arg instead of kwarg

--- tinyticker/web/web.py
+++ tinyticker/web/web.py
@@ -23,7 +23,7 @@ def create_app():
     @app.route("/")
     def index():
         config = config_file.read()
-        return render_template("index.html", config_file, **config)
+        return render_template("index.html", config_file=config_file, **config)
 
     @app.route("/config")
     def config():
