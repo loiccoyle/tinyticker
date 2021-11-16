99ab6a57096981e19cc498a85ce5d466badea67e
Loic Coyle <loic.coyle@hotmail.fr>
2021-11-16 17:12:45 +0100

    feat: add more epd models

--- tinyticker/waveshare_lib/__init__.py
+++ tinyticker/waveshare_lib/__init__.py
@@ -1,2 +1,7 @@
-# type: ignore
+"""The python interface for the ePaper displays.
+
+Adapted from:
+https://github.com/waveshare/e-Paper/tree/master/RaspberryPi_JetsonNano/python/lib/waveshare_epd
+"""
+
 from .epdconfig import CONFIG
--- tinyticker/waveshare_lib/models.py
+++ tinyticker/waveshare_lib/models.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Callable
 
-from . import epd2in13_V2, epd2in13b_V3
+from . import epd2in13, epd2in13_V2, epd2in13_V3, epd2in13b_V3, epd2in13bc
 
 
 @dataclass
@@ -16,6 +16,14 @@ class EPDModel:
 
 MODELS = [
     EPDModel(
+        name="EPD",
+        class_=epd2in13.EPD,
+        desc="Black and White 2.13 inch",
+        has_highlight=False,
+        width=epd2in13.EPD_WIDTH,
+        height=epd2in13.EPD_HEIGHT,
+    ),
+    EPDModel(
         name="EPD_v2",
         class_=epd2in13_V2.EPD,
         desc="Black and White 2.13 inch V2",
@@ -24,12 +32,28 @@ MODELS = [
         height=epd2in13_V2.EPD_HEIGHT,
     ),
     EPDModel(
+        name="EPD_v3",
+        class_=epd2in13_V3.EPD,
+        desc="Black, White 2.13 inch V3",
+        has_highlight=False,
+        width=epd2in13_V3.EPD_WIDTH,
+        height=epd2in13_V3.EPD_HEIGHT,
+    ),
+    EPDModel(
         name="EPDb_v3",
         class_=epd2in13b_V3.EPD,
         desc="Black, White and Red 2.13 inch V3",
         has_highlight=True,
         width=epd2in13b_V3.EPD_WIDTH,
         height=epd2in13b_V3.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPDbc",
+        class_=epd2in13bc.EPD,
+        desc="Black, White and Yellow 2.13 inch",
+        has_highlight=True,
+        width=epd2in13bc.EPD_WIDTH,
+        height=epd2in13bc.EPD_HEIGHT,
     ),
 ]
 MODELS = {model.name: model for model in MODELS}
