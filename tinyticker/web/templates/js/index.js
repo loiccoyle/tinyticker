f0199daafceecface1ea8ae1ef9f87b8c5faef74
Loic Coyle <loic.coyle@hotmail.fr>
2022-08-31 01:54:10 +0200

    feat(WIP): add multiple tickers to web app

--- tinyticker/web/templates/js/index.js
+++ tinyticker/web/templates/js/index.js
@@ -1,6 +1,10 @@
-function add_ticker(){
-    let tickers = document.getElementsByClassName("ticker");
-    let last_ticker = tickers[tickers.length - 1];
-    last_ticker.append(last_ticker.cloneNode(true));
-    return false
+function add_ticker() {
+  let tickers = document.getElementsByClassName("ticker");
+  let last_ticker = tickers[tickers.length - 1];
+  last_ticker.insertAdjacentElement("afterend", last_ticker.cloneNode(true));
+}
+
+function remove_ticker(element) {
+  let ticker = element.parentNode;
+  ticker.parentNode.removeChild(ticker);
 }
