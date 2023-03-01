fae529b358243558d3844bbf26d0799e5e30c5fb
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-01 19:15:10 +0100

    feat(web): make ticker list sortable

--- tinyticker/web/templates/js/index.js
+++ tinyticker/web/templates/js/index.js
@@ -1,7 +1,12 @@
 function add_ticker() {
   let tickers = document.getElementsByClassName("ticker");
   let last_ticker = tickers[tickers.length - 1];
-  last_ticker.insertAdjacentElement("afterend", last_ticker.cloneNode(true));
+  let new_ticker = last_ticker.cloneNode(true);
+  new_ticker.className += " uk-animation-slide-right";
+  new_ticker.addEventListener("animationend", function () {
+    new_ticker.classList.remove("uk-animation-slide-right");
+  });
+  last_ticker.insertAdjacentElement("afterend", new_ticker);
 }
 
 function remove_ticker(element) {
@@ -9,10 +14,9 @@ function remove_ticker(element) {
   if (tickers.length == 1) {
     return;
   }
-  let ticker = element.parentNode;
+  let ticker = element.parentNode.parentNode;
   ticker.addEventListener("animationend", function () {
     ticker.parentNode.removeChild(ticker);
   });
-  ticker.classList.remove("uk-animation-slide-right");
   ticker.className += " uk-animation-slide-top uk-animation-reverse";
 }
