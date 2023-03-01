e2a276b8db80a0e2ed922ed2dc96198aa2c19146
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-02 00:16:03 +0100

    test: add basic command.py test

---
+++ tests/unit/web/test_command.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from tinyticker.web import command
+
+
+class TestCommand(TestCase):
+    def test_register(self):
+        assert "new" not in command.COMMANDS.keys()
+
+        @command.register
+        def new():
+            pass
+
+        assert "new" in command.COMMANDS.keys()
+        assert command.COMMANDS["new"] == new
+
+    def test_try_command(self):
+        command.try_command("echo test")
+        command.try_command(["echo test"])
+        command.try_command("somecommandwhichdoesnotexist")
