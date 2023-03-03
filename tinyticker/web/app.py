904cc86c3e24df1e45920bdc2824f34af9125b46
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-04 00:45:56 +0100

    feat(web): run commands in a separate thread

--- tinyticker/web/app.py
+++ tinyticker/web/app.py
@@ -2,6 +2,7 @@ import logging
 import socket
 import subprocess
 import sys
+import threading
 from pathlib import Path
 from socket import timeout
 from typing import Optional
@@ -126,8 +127,10 @@ def create_app(config_file: Path = CONFIG_FILE, log_dir: Path = LOG_DIR) -> Flas
         LOGGER.debug("/command url args: %s", request.args)
         command = request.args.get("command")
         if command:
-            # call the registered command function
-            COMMANDS.get(command, lambda: None)()
+            # call the registered command function in a separate thread
+            cmd_func = COMMANDS.get(command, None)
+            if cmd_func is not None:
+                threading.Thread(target=cmd_func).start()
         return redirect("/", code=302)
 
     @app.route("/set_hostname")
