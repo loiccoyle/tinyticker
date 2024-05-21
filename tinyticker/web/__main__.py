504ad62afd2f35cf0aabe427601031fecdbf2b18
Loic Coyle <loic.coyle@hotmail.fr>
2024-05-21 03:52:40 +0200

    fix(web): use root logger & stop double logging

--- tinyticker/web/__main__.py
+++ tinyticker/web/__main__.py
@@ -14,10 +14,8 @@ from ..utils import (
     dashboard_qrcode,
     set_verbosity,
 )
-from .app import LOGGER as APP_LOGGER
+from . import logger
 from .app import create_app
-
-LOGGER = logging.getLogger(__name__)
 
 
 def parse_args(args: List[str]) -> argparse.Namespace:
@@ -72,17 +70,18 @@ def parse_args(args: List[str]) -> argparse.Namespace:
 
 def main():
     args = parse_args(sys.argv[1:])
+    # this stops the logger from double logging for some reason
+    logger.propagate = False
     if args.verbose > 0:
-        set_verbosity(LOGGER, args.verbose)
-        set_verbosity(APP_LOGGER, args.verbose)
+        set_verbosity(logger, args.verbose)
 
-    LOGGER.debug("Args: %s", args)
+    logger.debug("Args: %s", args)
 
     if not args.log_dir.is_dir():
         args.log_dir.mkdir(parents=True)
 
     if args.show_qrcode:
-        LOGGER.info("Generating qrcode.")
+        logger.info("Generating qrcode.")
         tt_config = TinytickerConfig.from_file(args.config)
         display = Display.from_tinyticker_config(tt_config)
         qrcode = dashboard_qrcode(
@@ -90,16 +89,16 @@ def main():
             display.epd.height,
             args.port,
         )
-        LOGGER.info("Displaying qrcode.")
+        logger.info("Displaying qrcode.")
         display.show_image(qrcode)
         del display
         sys.exit()
 
-    LOGGER.info("Starting tinyticker-web")
+    logger.info("Starting tinyticker-web")
     app = create_app(config_file=args.config, log_dir=args.log_dir)
     serve(app, port=args.port)
     # app.run(host="0.0.0.0", port=args.port, debug=False, threaded=True)
-    LOGGER.info("Stopping tinyticker-web")
+    logger.info("Stopping tinyticker-web")
 
 
 if __name__ == "__main__":
