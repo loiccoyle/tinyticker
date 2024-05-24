9d17ac4010ec35c845a3bc3e7b3332e3b7f16e56
Loic Coyle <loic.coyle@hotmail.fr>
2024-05-24 05:22:50 +0200

    fix(startup): actually run the scripts

--- tinyticker/web/startup.py
+++ tinyticker/web/startup.py
@@ -7,16 +7,14 @@ STARTUP_DIR = Path.home() / ".config" / "tinyticker" / "startup"
 
 
 def run_scripts():
-    scripts = STARTUP_DIR.glob("*")
+    scripts = [
+        file
+        for file in STARTUP_DIR.glob("*")
+        if file.is_file() and not file.name.startswith(".")
+    ]
     LOGGER.debug("Running startup scripts: %s", [file.name for file in scripts])
     processes = []
     for file in scripts:
-        if file.is_file() and not file.name.startswith("."):
-            LOGGER.debug("Running startup script: %s", file.name)
-            processes.append(
-                subprocess.Popen(
-                    f"./{file}",
-                    shell=True,
-                )
-            )
+        LOGGER.debug("Running startup script: %s", file.name)
+        processes.append(subprocess.Popen(f"{file}", shell=True))
     return processes
