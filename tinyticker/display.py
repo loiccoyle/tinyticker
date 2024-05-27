cded35eb1fbcfe304bf2a593d40dca5044859e52
Loic Coyle <loic.coyle@hotmail.fr>
2024-05-28 01:19:05 +0200

    fix(layouts): fall back to default

--- tinyticker/display.py
+++ tinyticker/display.py
@@ -126,6 +126,6 @@ class Display:
         self.epd.sleep()
 
     def show(self, ticker: TickerBase, resp: TickerResponse) -> None:
-        layout = LAYOUTS[ticker.config.layout.name]
+        layout = LAYOUTS.get(ticker.config.layout.name, LAYOUTS["default"])
         image = layout.func((self.epd.height, self.epd.width), ticker, resp)
         self.show_image(image)
