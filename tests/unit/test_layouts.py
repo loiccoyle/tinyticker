b7684fff8ee31bf70df199fe0d3a32ff423a5eb9
Loic Coyle <loic.coyle@hotmail.fr>
2024-05-28 01:41:33 +0200

    tests(layout): add test for layouts

--- tests/unit/test_layouts.py
+++ tests/unit/test_layouts.py
@@ -1,30 +1,49 @@
 import os
+from itertools import product
 from pathlib import Path
 from unittest import TestCase
 
 import pandas as pd
-from PIL import Image
 
 from tinyticker import layouts
 from tinyticker.config import TickerConfig
+from tinyticker.layouts import LayoutFunc
 from tinyticker.tickers import TickerCrypto
 from tinyticker.tickers._base import TickerResponse
 
 UPDATE_REF_PLOTS = os.environ.get("TINYTICKER_UPDATE_REF_PLOTS", False)
 
 
+def _test_layout(layout_func: LayoutFunc, dimensions, resp, data_dir):
+    config = TickerConfig(
+        symbol="SPY", interval="1d", lookback=30, plot_type="candle", volume=False
+    )
+    volume = [True, False]
+    y_axis = [True, False]
+    x_gaps = [True, False]
+    for volume, y_axis, x_gap in product(volume, y_axis, x_gaps):
+        config.volume = volume
+        config.layout.y_axis = y_axis
+        config.layout.x_gaps = x_gap
+
+        ticker = TickerCrypto("dummy key", config)
+        out = layout_func(dimensions, ticker, resp)
+        assert out.size == dimensions
+        filename = f"{layout_func.__name__}_{y_axis}_{x_gap}_{volume}.jpg"
+        if UPDATE_REF_PLOTS:
+            out.save(data_dir / filename)
+        assert out.tobytes() == (data_dir / filename).read_bytes() or UPDATE_REF_PLOTS
+
+
 class TestLayout(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data_dir = Path(__file__).parents[1] / "data"
+        cls.layout_dir = cls.data_dir / "layouts"
         cls.config_path = cls.data_dir / "config.json"
         cls.historical: pd.DataFrame = pd.read_pickle(
-            cls.data_dir / "crypto_historical.pkl"
+            cls.data_dir / "stock_historical.pkl"
         )
-        cls.default = Image.open(cls.data_dir / "layout_default.png").convert("RGB")
-        cls.default_volume = Image.open(
-            cls.data_dir / "layout_default_volume.png"
-        ).convert("RGB")
 
         cls.dimensions = (250, 122)
         cls.resp = TickerResponse(cls.historical, cls.historical.iloc[-1]["Close"])
@@ -38,21 +57,7 @@ class TestLayout(TestCase):
         assert (fig.get_size_inches() * fig.dpi == self.dimensions).all()
 
     def test_default(self):
-        config = TickerConfig(
-            symbol="BTC", interval="1h", plot_type="candle", lookback=24, volume=False
-        )
+        _test_layout(layouts.default, self.dimensions, self.resp, self.layout_dir)
 
-        ticker = TickerCrypto("dummy key", config)
-        img = layouts.default(self.dimensions, ticker, self.resp)
-        assert img.size == self.dimensions
-        if UPDATE_REF_PLOTS:
-            img.save(self.data_dir / "layout_default.png")
-        assert img.tobytes() == self.default.tobytes() or UPDATE_REF_PLOTS
-
-        config.volume = True
-        ticker = TickerCrypto("dummy key", config)
-        img = layouts.default(self.dimensions, ticker, self.resp)
-        assert img.size == self.dimensions
-        if UPDATE_REF_PLOTS:
-            img.save(self.data_dir / "layout_default_volume.png")
-        assert img.tobytes() == self.default_volume.tobytes() or UPDATE_REF_PLOTS
+    def test_big_price(self):
+        _test_layout(layouts.big_price, self.dimensions, self.resp, self.layout_dir)
