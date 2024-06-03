80ffef127a989e59eb0f6bc0258a183e7a956d16
Loic Coyle <loic.coyle@hotmail.fr>
2024-06-03 22:19:45 +0200

    fix(epd2in7_V2): use `send_data2`

--- tinyticker/waveshare_lib/epd2in7_V2.py
+++ tinyticker/waveshare_lib/epd2in7_V2.py
@@ -437,15 +437,8 @@ class EPD(EPDMonochrome):
         self.TurnOnDisplay()
 
     def display(self, image):
-        if self.width % 8 == 0:
-            Width = self.width // 8
-        else:
-            Width = self.width // 8 + 1
-        Height = self.height
         self.send_command(0x24)
-        for j in range(Height):
-            for i in range(Width):
-                self.send_data(image[i + j * Width])
+        self.send_data2(image)
         self.TurnOnDisplay()
 
     def display_Fast(self, image):
