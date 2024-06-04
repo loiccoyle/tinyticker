c3908a07e50ae481806f5aa40b8c6c63e5de636a
Loic Coyle <loic.coyle@hotmail.fr>
2024-06-04 02:29:52 +0200

    fix: set `PIL.open` format

--- tinyticker/layouts.py
+++ tinyticker/layouts.py
@@ -93,10 +93,10 @@ def _fig_to_image(fig: Figure) -> Image.Image:
     """
     fig.tight_layout(pad=0)
     with io.BytesIO() as buffer:
-        fig.savefig(buffer, format="jpg", pad_inches=0)
+        fig.savefig(buffer, format="jpeg", pad_inches=0)
         # to stop the fig from showing up in notebooks and such
         plt.close(fig)
-        return Image.open(buffer).convert("RGB")
+        return Image.open(buffer, formats=("jpeg",)).convert("RGB")
 
 
 def register(func: LayoutFunc) -> LayoutFunc:
