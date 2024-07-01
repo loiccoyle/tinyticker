f643b4320e24419d0cf8e71993d7ac1647c482cd
Loic Coyle <loic.coyle@hotmail.fr>
2024-07-01 17:20:38 +0200

    fix(waveshare): use l2 norm

--- tinyticker/waveshare_lib/_base.py
+++ tinyticker/waveshare_lib/_base.py
@@ -217,8 +217,9 @@ class EPDGrayscale(EPDMonochrome):
         # loss when displaying in bit mode
         loss = np.linalg.norm(
             np.array(image.convert("L")) / 255
-            - np.array(image.convert("1", dither=None))
-        ) / (image.size[0] * image.size[1])
+            - np.array(image.convert("1", dither=None)),
+            ord=2,
+        ) / (image.size[0] * image.size[1],)
         logger.debug("grayscale bitmap loss: %f", loss)
         threshold = 1e-4
 
